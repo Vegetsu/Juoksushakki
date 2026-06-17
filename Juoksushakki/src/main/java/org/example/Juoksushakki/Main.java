67970dcb6f95088399afdc524ebaf4faa55051eb
Vegetsu <veikka.vetola@gmail.com>
2026-06-17 14:01:07 +0300

    Add files via upload

---
+++ Juoksushakki/src/main/java/org/example/Juoksushakki/Main.java
@@ -0,0 +1,10 @@
+package org.example.Juoksushakki;
+
+
+/*
+Käynnistää shakkilautasovelluksen
+ */
+public class Main {
+        public static void main(String[] args) {
+            Shakkilauta.main(args);}
+}
